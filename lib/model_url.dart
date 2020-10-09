48a084855ccd8f3a9598132bf07de9cbb0cca3dd
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2020-10-09 20:05:27 +1100

    Add model urls

---
+++ lib/model_url.dart
@@ -0,0 +1,15 @@
+Map<String, String> modelURL = {
+  'en': "http://alphacephei.com/vosk/models/vosk-model-small-en-us-0.4.zip",
+  'en_in': "http://alphacephei.com/vosk/models/vosk-model-small-en-in-0.4.zip",
+  'cn': "https://alphacephei.com/vosk/models/vosk-model-small-cn-0.3.zip",
+  "ru": "https://alphacephei.com/vosk/models/vosk-model-small-ru-0.4.zip",
+  "fr":
+      "https://alphacephei.com/vosk/models/vosk-model-small-fr-pguyot-0.3.zip",
+  "de": "https://alphacephei.com/vosk/models/vosk-model-small-de-zamia-0.3.zip",
+  "es": "https://alphacephei.com/vosk/models/vosk-model-small-es-0.3.zip",
+  "tr": "https://alphacephei.com/vosk/models/vosk-model-small-tr-0.3.zip",
+  "vn": "https://alphacephei.com/vosk/models/vosk-model-small-vn-0.3.zip",
+  "it": "https://alphacephei.com/vosk/models/vosk-model-small-it-0.4.zip",
+  "ca": "https://alphacephei.com/vosk/models/vosk-model-small-ca-0.4.zip",
+  "fa": "https://alphacephei.com/vosk/models/vosk-model-small-fa-0.4.zip"
+};
